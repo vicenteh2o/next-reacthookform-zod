f62df47aa4bed7a0b71906a967ef4083e4a69c65
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-06-13 08:32:27 -0300

    feat: validation with hook form and zod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,55 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userSchema, mappedPlans } from "@/schema/userSchema";
+
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(userSchema),
+  });
+
+  const planOptions = Object.entries(mappedPlans).map(([key, value]) => (
+    <option value={key} key={`plan-${key}`}>
+      {value}
+    </option>
+  ));
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" {...register("name")} />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
+        <input type="password" id="password" {...register("password")} />
 
         <label htmlFor="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" name="confirmPassword" />
+        <input
+          type="password"
+          id="confirmPassword"
+          {...register("confirmPassword")}
+        />
 
         <label htmlFor="weight">Weight</label>
-        <input type="number" id="weight" name="weight" />
+        <input type="number" id="weight" {...register("weight")} />
 
         <label htmlFor="plan">Plan</label>
-        <select name="plan" id="plan">
-          <option value="free">Free</option>
-          <option value="basic">Basic</option>
-          <option value="medium">Medium</option>
-          <option value="premium">Premium</option>
+        <select {...register("plan")} id="plan">
+          {planOptions}
         </select>
 
         <button type="submit">Submit</button>
       </form>
+      <div>{JSON.stringify(watch(), null, 2)}</div>
     </div>
   );
 }
---
+++ src/schema/userSchema.tsx
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+const plans = ["free", "basic", "medium", "premium"] as const;
+export type Plans = (typeof plans)[number];
+export const mappedPlans: { [key in Plans]: string } = {
+  basic: "Basic",
+  free: "Free",
+  medium: "Medium",
+  premium: "Pro",
+};
+
+export const userSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Name must be at least 6 char long")
+    .max(10, "Name must be less than 10 char long"),
+  email: z.string().email("Please enter a valid email"),
+  password: z.string().min(6, "Name must be at least 6 char long"),
+  confirmPassword: z.string().min(6, "Name must be at least 6 char long"),
+  weight: z
+    .string()
+    .refine((value) => !isNaN(parseFloat(value)), "Weight must be a number"),
+  plan: z.enum(plans, {
+    errorMap: () => ({ message: "Please select a plan" }),
+  }),
+});
